084a368b19134bfc2e36aa5c2dc2662c79c987c6
PetrovDenisC <73660206+PetrovDenisC@users.noreply.github.com>
2021-02-09 21:43:15 +0300

    Add_React tec
    Add_React tec

---
+++ Album/2/script.js
@@ -0,0 +1,79 @@
+class Slider extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { curentSlide: 0 };        
+        this.handleNext = this.handleNext.bind(this);
+        this.handlePrev = this.handlePrev.bind(this);
+        this.handleNav = this.handleNav.bind(this);
+    }
+
+    handleNext(){
+        if(this.state.curentSlide >= this.props.items.length -1 ){
+            return;
+        };
+        this.setState(state =>({
+            curentSlide: state.curentSlide + 1
+        }));
+    }
+    handlePrev(){
+        if(this.state.curentSlide <= 0){
+            return;
+        };
+        this.setState(state =>({
+            curentSlide: state.curentSlide -1
+        }));
+    }
+    handleNav(e){
+        let id=e.target.dataset.id-1;
+        if(this.state.curentSlide==id){
+            return;
+        };
+        this.setState({curentSlide:id})
+    }
+
+
+    render(){
+        return(
+            <div className="slider">
+                <button className="left-control" onClick={this.handlePrev}></button>
+                <button className="right-control" onClick={this.handleNext}></button>
+                <div className="nav-control">
+                    {this.props.items.map(item =>(
+                        <button className="newBtn" key={item.id} data-id={item.id} onClick={this.handleNav}></button>
+                    ))}
+                </div>
+                <div className="item-list" style={{ 'transform': 'translate(-' + this.state.curentSlide*100+'%)' }}>
+                    {this.props.items.map(item =>(
+                        <div className="item" key={item.id}>
+                            <img src={item.img} alt={item.text}/>
+                            <h3 className='item-text'>{item.text}</h3>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+const items =[
+    {
+        id:1,
+        img:'slide.jpg',
+        text:'First slide',
+    },
+    {
+        id:2,
+        img:'slide.jpg',
+        text:'Second slide',
+    },
+    {
+        id:3,
+        img:'slide.jpg',
+        text:'Third slide',
+    },
+]
+
+ReactDOM.render(
+    <Slider items = {items} />,
+    document.getElementById('root')
+)
